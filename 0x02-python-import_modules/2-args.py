87af0e0c0894efa27a231b5ed109813e859fb656
unyime-montag <unyime.ekanem@hotmail.com>
2021-11-18 18:55:10 -0800

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+    leng = len(sys.argv)
+    if leng == 1:
+        print('{} arguments.'.format(leng - 1))
+    elif len(sys.argv) == 2:
+        print('{} argument:'.format(leng - 1))
     else:
-        print("{} arguments:".format(count))
-        for i in range(count):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print('{} arguments:'.format(leng - 1))
+    for i in range(leng):
+        if i == 0:
+            continue
+        print('{:d}: {:s}'.format(i, sys.argv[i]))
