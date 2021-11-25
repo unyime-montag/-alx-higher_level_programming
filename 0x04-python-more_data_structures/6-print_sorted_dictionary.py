e27a613035375c9969e6ad7afb9fbc631b5b19fe
unyime-montag <unyime.ekanem@hotmail.com>
2021-11-24 18:28:42 -0800

    more data

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-        """
-    Prints a dictionary by ordered keys
-    ...
-    Parameters
-    ----------
-    a_dictionary : dictionary
-        the given dictionary
-    Return:
-        Nothing
-    """
 
-            [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
+
+def print_sorted_dictionary(a_dictionary):
+    """
+    A function that prints a dictionary by ordered keys
+    """
+    keys = list(a_dictionary.keys())
+    keys.sort()
+    for key in keys:
+        print("{}: {}".format(key, a_dictionary[key]))
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-        """
-    Returns a new dictionary with all values multiplied by 2
-    ...
-    Parameters
-    ----------
-    a_dictionary : dictionary
-        the given dictionary
-    Return:
-        a new dictionary
-    """
 
-            return ({k: a_dictionary[k] * 2 for k in a_dictionary})
+
+def multiply_by_2(a_dictionary):
+    """
+    A function that returns a new dictionary
+    with all values multiplied by 2
+    """
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict.update({key: (value * 2)})
+    return new_dict
