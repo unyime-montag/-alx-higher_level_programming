993e316c32fc6ac04760a95c6b5ec96e4ba3d1fb
unyime-montag <unyime.ekanem@hotmail.com>
2021-12-07 11:45:23 -0800

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class Square"""
+11;rgb:0000/0000/0000
+
+class Square:
+        """Class Square"""
+            def __init__(self, size=0):
+                        """[summary]
+        
+        Keyword Arguments:
+            size {int} -- size of the Square (default: {0})
+        """
+                                self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """Class Square
+        
+        Keyword Arguments:
+            size {int} -- size of square(default: {0})
+        
+        Raises:
+            ValueError: must be > 0
+            TypeError: must be an int
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
