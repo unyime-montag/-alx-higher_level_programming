4016f106029bd2f468b56709ce959d5d9007d42d
unyime-montag <unyime.ekanem@hotmail.com>
2022-02-28 13:20:58 -0800

    JS is js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(s[i]);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+for (array[i]; i < array.length; i++) {
+  console.log(array[i]);
 }
